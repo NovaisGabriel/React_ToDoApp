f77a4e2780774fd6f78d7d487c785d7c62cd3b77
NovaisGabriel <gabrieel.novais@gmail.com>
2020-05-26 18:57:22 -0300

    Funções de Handle do formulário

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name ="Sobre" small=" About"></PageHeader>
+
+        <h2>Lorem Ipsum</h2>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida ligula in neque dapibus, id congue libero facilisis. Sed vel ultricies turpis. Nunc at ex a tellus pulvinar volutpat a et ligula. Vivamus sed leo ut neque faucibus facilisis. Sed congue arcu at augue malesuada pulvinar. Vestibulum tempus rhoncus porta. Vestibulum molestie vitae dolor eu ullamcorper. Cras sit amet libero at urna ultricies sollicitudin fringilla id tortor. Mauris in ipsum dui.
+
+Cras lectus ex, interdum vitae eros in, convallis rutrum lorem. Curabitur nec nulla vel lacus tempus porttitor. Morbi ut iaculis erat. Phasellus vitae risus a orci ornare malesuada et sit amet nulla. Aliquam a tincidunt diam. Vivamus eget augue enim. Pellentesque feugiat, odio vitae fringilla sodales, augue dolor egestas dolor, at volutpat lectus purus ultricies mi. Ut eu tincidunt ex. Donec iaculis, nibh vitae lobortis tempus, eros tellus lobortis augue, vel ultrices neque velit nec ex. Duis ex ante, semper consequat tincidunt at, ullamcorper non orci. Nam vitae dictum neque. Vivamus magna mauris, tristique ac cursus non, finibus eu dolor. Morbi massa ante, ullamcorper vitae tempor et, cursus vel nisi. Morbi vitae nisl a lacus auctor molestie.
+
+Fusce interdum lorem nec urna efficitur ultrices. Praesent eu purus eget elit posuere cursus eu luctus dui. Sed vel purus at quam vestibulum dapibus a ac quam. Cras a mollis nisl. Aenean pretium auctor elit, eget ultricies velit porta ut. Vivamus enim mi, lobortis nec ligula sit amet, euismod congue enim. Vivamus tempor ipsum eget risus dictum vehicula. Donec porttitor pellentesque sem quis semper. Aliquam placerat arcu eleifend nunc maximus, nec commodo ipsum porttitor.
+
+In nec mi quis diam tincidunt varius vitae non magna. Ut nisl nisi, cursus sed odio a, sodales condimentum magna. Nulla eu tincidunt erat. Sed tincidunt laoreet ante, vitae dictum sem. In volutpat rutrum tellus eget suscipit. Nullam odio turpis, dictum id lorem sit amet, hendrerit facilisis eros. Sed libero elit, posuere in nunc at, gravida ultrices augue. Suspendisse et tortor nec massa posuere cursus at in ante. Phasellus vel mi ut diam imperdiet maximus. Donec vel nibh luctus, iaculis ligula gravida, posuere mi. Nulla facilisi. Nunc iaculis semper velit, quis porttitor eros congue vel. Etiam suscipit lectus augue, ut blandit felis condimentum ut.
+
+Fusce luctus lacus est, vel suscipit turpis tincidunt sed. Ut vel rhoncus magna. Curabitur tincidunt auctor nibh, non vehicula tortor hendrerit eu. Vivamus sed nunc ac orci bibendum faucibus. Curabitur quis convallis ante. Nam porttitor ex at cursus vehicula. Quisque quis augue eget urna feugiat pretium vel vitae erat. Vivamus porttitor, risus et faucibus porta, dolor elit convallis mi, eu consectetur nisl eros non velit. Ut magna enim, aliquet a rhoncus vitae, eleifend non ipsum. Vivamus at hendrerit massa. Mauris vitae mi urna.
+
+Interdum et malesuada fames ac ante ipsum primis in faucibus. In a nunc interdum, egestas ex eget, blandit risus. Etiam vulputate, augue sed egestas mattis, odio velit faucibus leo, sed volutpat tellus purus eu mi. Donec molestie nisi a arcu pretium venenatis. Vestibulum purus tellus, elementum id suscipit et, facilisis id sem. Maecenas interdum hendrerit dapibus. Morbi blandit sollicitudin ornare. Integer pharetra elit eget velit pulvinar, nec sollicitudin eros iaculis. Cras malesuada, leo nec facilisis dignissim, velit justo sagittis ante, sit amet consectetur orci lacus a sem. Maecenas sit amet magna sed erat consequat tincidunt ac non quam. Fusce laoreet erat nec felis imperdiet varius. Integer laoreet nisi viverra, dictum arcu fermentum, dignissim orci. Nulla laoreet nibh arcu, quis placerat tortor tempor hendrerit. Nunc finibus, ipsum nec blandit euismod, lorem diam facilisis lectus, a viverra lectus urna et ligula. Proin sodales odio sed tristique tristique.
+
+Integer non odio in mi dapibus iaculis. Ut sodales magna quis nibh sollicitudin tincidunt. Cras eu arcu porta, molestie metus quis, faucibus tortor. Integer pellentesque, arcu id venenatis egestas, elit lorem mollis nunc, eget varius elit magna ultrices velit. Aliquam placerat feugiat odio a vehicula. Maecenas eleifend nisi maximus, dictum nisl non, auctor lacus. Quisque imperdiet erat ut nibh sollicitudin, non mattis ipsum ultrices. Pellentesque ultrices eros tellus, quis imperdiet velit pulvinar id. Etiam posuere fringilla diam nec molestie. Donec eget felis eget diam rhoncus scelerisque. Duis erat diam, egestas vitae diam ut, porttitor egestas mi. Integer tellus dolor, congue a quam varius, ultricies tristique libero.</p>
+
+    </div>
+)
